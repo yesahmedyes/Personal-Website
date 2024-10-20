c98ef489726a4a811a2a4de7048ad149a0706cc4
Ahmed Haroon <ahmed.dys99@gmail.com>
2024-10-21 01:47:38 +0500

    starting naive bayes

---
+++ src/app/notes/machine-learning/generative-learning-algorithms/components/naiveBayes.tsx
@@ -0,0 +1,12 @@
+import Content from "~/app/notes/_components/content";
+import Section from "~/app/notes/_components/section";
+
+export default function NaiveBayes() {
+  return (
+    <Section heading="Naive Bayes">
+      <Content>
+        <div></div>
+      </Content>
+    </Section>
+  );
+}
--- src/app/notes/machine-learning/generative-learning-algorithms/page.tsx
+++ src/app/notes/machine-learning/generative-learning-algorithms/page.tsx
@@ -1,10 +1,12 @@
 import MainWithSidebar from "../../_components/mainWithSidebar";
 import GDA from "./components/gda";
+import NaiveBayes from "./components/naiveBayes";
 
 export default function GenerativeLearningAlgorithms() {
   return (
     <MainWithSidebar>
       <GDA />
+      <NaiveBayes />
     </MainWithSidebar>
   );
 }
