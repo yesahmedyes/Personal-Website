ace939172bd1f3fa79abae890c7c351a3ff9c461
Ahmed Haroon <ahmed.dys99@gmail.com>
2024-10-22 17:32:09 +0500

    valid kernels added

--- src/app/notes/machine-learning/kernel-methods/page.tsx
+++ src/app/notes/machine-learning/kernel-methods/page.tsx
@@ -1,10 +1,12 @@
 import MainWithSidebar from "../../_components/mainWithSidebar";
 import KernelsMethods from "./components/kernelMethods";
+import ValidKernels from "./components/validKernels";
 
 export default function Page() {
   return (
     <MainWithSidebar>
       <KernelsMethods />
+      <ValidKernels />
     </MainWithSidebar>
   );
 }
