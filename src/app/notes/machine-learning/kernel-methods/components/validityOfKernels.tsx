b281aced9b932ff52a08373f79a6bf518e2ca16c
Ahmed Haroon <ahmed.dys99@gmail.com>
2024-10-22 15:11:45 +0500

    kernel methods added

---
+++ src/app/notes/machine-learning/kernel-methods/components/validityOfKernels.tsx
@@ -0,0 +1,15 @@
+import { BlockMath, InlineMath } from "react-katex";
+import Content from "~/app/notes/_components/content";
+import Derivation from "~/app/notes/_components/derivation";
+import DerivationContent from "~/app/notes/_components/derivationContent";
+import Section from "~/app/notes/_components/section";
+
+export default function ValidityOfKernels() {
+  return (
+    <Section title="Validity of Kernels">
+      <Content>
+        <div></div>
+      </Content>
+    </Section>
+  );
+}
---
+++ src/app/notes/machine-learning/kernel-methods/page.tsx
@@ -0,0 +1,10 @@
+import MainWithSidebar from "../../_components/mainWithSidebar";
+import KernelsMethods from "./components/kernelMethods";
+
+export default function Page() {
+  return (
+    <MainWithSidebar>
+      <KernelsMethods />
+    </MainWithSidebar>
+  );
+}
