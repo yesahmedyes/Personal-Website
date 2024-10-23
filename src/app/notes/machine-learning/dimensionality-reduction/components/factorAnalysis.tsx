c27e3f8a43b774f16df80ebc8abb0060d0d955a6
Ahmed Haroon <ahmed.dys99@gmail.com>
2024-10-24 03:59:29 +0500

    factor analysis more derivations

--- src/app/notes/machine-learning/dimensionality-reduction/components/factorAnalysis.tsx
+++ src/app/notes/machine-learning/dimensionality-reduction/components/factorAnalysis.tsx
@@ -6,6 +6,7 @@ import Detour from "~/app/notes/_components/detour";
 import Derivation from "~/app/notes/_components/derivation";
 import DerivationContent from "~/app/notes/_components/derivationContent";
 import Info from "~/app/notes/_components/info";
+import Lemma from "~/app/notes/_components/lemma";
 export default function FactorAnalysis() {
   return (
     <Section heading="Factor Analysis">
@@ -120,6 +121,138 @@ export default function FactorAnalysis() {
         <div>
           However, there does not exist any closed form solution for the maximum likelihood estimate of <InlineMath math="\mu, W, \Psi" />. This is because the matrices <InlineMath math="W" /> and{" "}
           <InlineMath math="\Psi" /> are coupled in the likelihood function and therefore, we cannot optimize them separately.
+        </div>
+        <div>
+          Therefore, we need to use the <MyLink href="/notes/machine-learning/em-algorithm">Expectation Maximization</MyLink> algorithm instead.
+        </div>
+        <div>
+          For the <InlineMath math="E" />
+          -step, we need to find the estimate for our posterior distribution of <InlineMath math="z" /> given <InlineMath math="x" />. This distribution can be written as:
+        </div>
+        <div className="flex flex-col">
+          <BlockMath math="z|x \sim \mathcal{N}\left(\mu_{z|x}, \, \Sigma_{z|x}\right)" />
+          <BlockMath math="\mu_{z|x} = W^T \left( WW^T + \Psi \right)^{-1} \left( x - \mu \right)" />
+          <BlockMath math="\Sigma_{z|x} = I - W^T \left( WW^T + \Psi \right)^{-1} W" />
+        </div>
+      </Content>
+      <Derivation>
+        <Lemma>
+          <div>
+            For any two gaussian distributions <InlineMath math="z" /> and <InlineMath math="x" />, the conditional distribution <InlineMath math="z|x" /> is also a gaussian distribution and is given
+            by:
+          </div>
+          <div className="flex flex-col">
+            <BlockMath math="z | x \sim \mathcal{N}\left(\mu_{z|x}, \Sigma_{z|x}\right)" />
+            <BlockMath math="\mu_{z|x} = \mu_z + \Sigma_{zx} \Sigma_{xx}^{-1} (x - \mu_x)" />
+            <BlockMath math="\Sigma_{z|x} = \Sigma_{zz} - \Sigma_{zx} \Sigma_{xx}^{-1} \Sigma_{xz}" />
+          </div>
+        </Lemma>
+        <DerivationContent>
+          <div>From our original equations, we can see that:</div>
+          <div className="flex flex-col">
+            <BlockMath math="\mu_z = 0" />
+            <BlockMath math="\Sigma_{zz} = \text{Var}(z) = I" />
+          </div>
+        </DerivationContent>
+        <DerivationContent>
+          <div>
+            Later to find the distribution of <InlineMath math="x" />, we saw that:
+          </div>
+          <div className="flex flex-col">
+            <BlockMath math="\mu_x = \mu" />
+            <BlockMath math="\Sigma_{xx} = WW^T + \Psi" />
+          </div>
+        </DerivationContent>
+        <DerivationContent>
+          <div>Now,</div>
+          <div className="flex flex-col">
+            <BlockMath math="\Sigma_{zx} = \text{Cov}(z, x) = \mathbb{E} \left[(z - \mathbb{E}[z])(x - \mathbb{E}[x])^T \right]" />
+            <Info
+              info={
+                <div>
+                  <InlineMath math="\mathbb{E}[z] = 0" /> and <InlineMath math="\mathbb{E}[x] = \mu" />
+                </div>
+              }
+            >
+              <BlockMath math="= \mathbb{E} \left[(z - 0)(x - \mu)^T \right]" />
+            </Info>
+            <Info
+              info={
+                <div>
+                  <InlineMath math="x = \mu + Wz + \epsilon" />
+                </div>
+              }
+            >
+              <BlockMath math="= \mathbb{E} \left[z(\mu + Wz + \epsilon - \mu)^T \right]" />
+            </Info>
+            <BlockMath math="= \mathbb{E} \left[z z^T W^T \right] + \mathbb{E} \left[z \epsilon^T \right]" />
+            <Info
+              info={
+                <div className="flex flex-col">
+                  <div>
+                    Since <InlineMath math="z" /> and <InlineMath math="\epsilon" /> are independent.
+                  </div>
+                  <div>
+                    Thus, <InlineMath math="\mathbb{E}\left[z \epsilon^T \right] = \mathbb{E}[z] \cdot \mathbb{E}[\epsilon^T] = 0" />.
+                  </div>
+                </div>
+              }
+            >
+              <BlockMath math="= \mathbb{E} \left[z z^T \right] \cdot W^T + 0" />
+            </Info>
+            <Info
+              info={
+                <div>
+                  <BlockMath math="\mathbb{E}\left[z z^T \right] = \text{Var}(z) = I" />
+                </div>
+              }
+            >
+              <BlockMath math="= W^T" />
+            </Info>
+          </div>
+        </DerivationContent>
+        <DerivationContent>
+          <div>Similarly,</div>
+          <div className="flex flex-col">
+            <BlockMath math="\Sigma_{xz} = \text{Cov}(x, z) = W" />
+          </div>
+        </DerivationContent>
+        <DerivationContent>
+          <div>Putting this all together, we get:</div>
+          <div className="flex flex-col">
+            <BlockMath math="z|x \sim \mathcal{N}\left(\mu_{z|x}, \, \Sigma_{z|x}\right)" />
+            <BlockMath math="\mu_{z|x} = W^T \left( WW^T + \Psi \right)^{-1} \left( x - \mu \right)" />
+            <BlockMath math="\Sigma_{z|x} = I - W^T \left( WW^T + \Psi \right)^{-1} W" />
+          </div>
+        </DerivationContent>
+      </Derivation>
+      <Content>
+        <div>
+          Now, the <InlineMath math="E" />
+          -step for our EM algorithm is given by:
+        </div>
+        <div className="flex flex-col">
+          <BlockMath math="Q_i(z^{(i)}) = p\left(z^{(i)} | x^{(i)}; \mu, W, \Psi\right)" />
+          <BlockMath math="= \frac{1}{(2\pi)^{k/2} |\Sigma_{z^{(i)}|x^{(i)}}|^{1/2}} \exp\left( -\frac{1}{2} \left( z^{(i)} - \mu_{z^{(i)}|x^{(i)}} \right)^T \Sigma_{z^{(i)}|x^{(i)}}^{-1} \left( z^{(i)} - \mu_{z^{(i)}|x^{(i)}} \right) \right)" />
+        </div>
+      </Content>
+      <Content>
+        <div>
+          In the <InlineMath math="M" />
+          -step, we need to maximize our evidence lower bound:
+        </div>
+        <div>
+          <BlockMath math="\text{ELBO}\left(x; Q, \mu, W, \Psi \right) = \sum_{i=1}^n \mathbb{E}_{z^{(i)} \sim Q_i} \left[ \log \frac{p\left(x^{(i)}, z^{(i)}; \mu, W, \Psi\right)}{Q_i(z^{(i)})} \right]" />
+        </div>
+      </Content>
+      <Content>
+        <div>
+          Maximizing the <InlineMath math="\text{ELBO}" /> with respect to each of the parameters <InlineMath math="\mu, W, \Psi" />, we get the following update equations:
+        </div>
+        <div className="flex flex-col">
+          <BlockMath math="\mu = \frac{1}{n} \sum_{i=1}^n x^{(i)}" />
+          <BlockMath math="W = \left( \sum_{i=1}^{n} (x^{(i)} - \mu) \mu_{z^{(i)}|x^{(i)}}^T \right) \left( \sum_{i=1}^{n} \mu_{z^{(i)}|x^{(i)}} \mu_{z^{(i)}|x^{(i)}}^T + \Sigma_{z^{(i)}|x^{(i)}} \right)^{-1}" />
+          <BlockMath math="\Phi = \frac{1}{n} \sum_{i=1}^{n} \left( x^{(i)} {x^{(i)}}^T - x^{(i)} \mu_{z^{(i)}|x^{(i)}}^T W^T - W \mu_{z^{(i)}|x^{(i)}} {x^{(i)}}^T + W \left( \mu_{z^{(i)}|x^{(i)}} \mu_{z^{(i)}|x^{(i)}}^T + \Sigma_{z^{(i)}|x^{(i)}} \right) W^T \right)" />
         </div>
       </Content>
     </Section>
