d389f591e532d05bcaae3b2e5df1d591681e57ce
Ahmed Haroon <ahmed.dys99@gmail.com>
2024-10-23 17:36:54 +0500

    factor analysis intro added

--- src/app/notes/machine-learning/dimensionality-reduction/page.tsx
+++ src/app/notes/machine-learning/dimensionality-reduction/page.tsx
@@ -1,10 +1,12 @@
 import MainWithSidebar from "../../_components/mainWithSidebar";
+import FactorAnalysis from "./components/factorAnalysis";
 import PCA from "./components/pca";
 
 export default function Page() {
   return (
     <MainWithSidebar>
       <PCA />
+      <FactorAnalysis />
     </MainWithSidebar>
   );
 }
